bae08d8596eed541ee85b9f905e0adb5ef16938a
Ling <vumanhling@icloud.com>
2019-11-11 14:13:05 +0100

    Add new style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hello';
+  color = 'rgba(50, 115, 220, 0.3)';
+
+  changeColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    this.color = `rgba(${r}, ${g}, ${b}, 0.3)`;
+  }
 }
