c78b577faf357a0d31b1b5a9b34a06b08de64e49
Manh Ling Vu <vumanhling@icloud.com>
2020-10-03 16:44:01 +0200

    Remove deprecated repo and renew

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CoverComponent } from './pages/cover/cover.component';
+
+const routes: Routes = [
+  { path: '', component: CoverComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  color = 'rgba(50, 115, 220, 0.3)';
-
-  changeColor() {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    this.color = `rgba(${r}, ${g}, ${b}, 0.3)`;
-  }
-}
+export class AppComponent {}
